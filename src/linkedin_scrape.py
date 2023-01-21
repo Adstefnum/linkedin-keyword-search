9b349aae76d2bd132e1a085e8678b6b8932f5b9a
Stephen Adesina <bauerstevenson@gmail.com>
2023-01-21 07:18:38 +0200

    added link to get people

--- src/linkedin_scrape.py
+++ src/linkedin_scrape.py
@@ -1,3 +1,3 @@
 import requests
 
-response = requests.get("https://www.linkedin.com/search/results/all/?keywords=CTO&Industrial Company&Europe")
+response = requests.get("https://www.linkedin.com/search/results/people/?keywords=%22CTO%22%20AND%20%22Industrial%20Company%22%20AND%20%22Europe%22&origin=SWITCH_SEARCH_VERTICAL&sid=XhJ")
