624e9777d0acf23d238579ea540ef964cb819d6c
Stephen Adesina <bauerstevenson@gmail.com>
2023-01-22 19:49:59 +0000

    code control

--- src/main.py
+++ src/main.py
@@ -2,13 +2,20 @@ from tabulate import tabulate
 from linkedin_scrape import LinkedinScraper
 from linkedin_parser import LinkedinParser
 
+
 #filters
-scraper = LinkedinScraper()
-scraper.login_to_linkedin()
-soup = scraper.retrieve_soup("https://www.linkedin.com/search/results/people/?keywords=%22CTO%22%20AND%20%22Industrial%20Company%22%20AND%20%22Europe%22&origin=SWITCH_SEARCH_VERTICAL&sid=XhJ")
 
-parser = LinkedinParser(soup)
-results = parser.process_profile_lists()
+def run():
+    scraper = LinkedinScraper()
+    scraper.login_to_linkedin()
+    soup = scraper.retrieve_soup("https://www.linkedin.com/search/results/people/?keywords=%22CTO%22%20AND%20%22Industrial%20Company%22%20AND%20%22Europe%22&origin=SWITCH_SEARCH_VERTICAL&sid=XhJ")
+
+    parser = LinkedinParser(soup)
+    results = parser.process_profile_lists()
 
 
-print(tabulate(results, headers='firstrow', tablefmt='fancy_grid'))
+    print(tabulate(results, headers='firstrow', tablefmt='fancy_grid'))
+
+if __name__ == '__main__':
+    run()
+
