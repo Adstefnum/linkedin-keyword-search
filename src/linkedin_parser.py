d2d25aa3a45d3de85757afc5aa363f9fc6c9cec8
Stephen Adesina <bauerstevenson@gmail.com>
2023-01-22 19:21:18 +0000

    template

---
+++ src/linkedin_parser.py
@@ -0,0 +1,10 @@
+NAME_XPATH = 
+ID_XPATH = 
+COMPANY_XPATH = 
+
+class LinkedinParser:
+
+    def __init__(self, soup):
+        self.soup = soup
+
+
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from tabulate import tabulate
+
+table = [['col 1', 'col 2', 'col 3', 'col 4'], [1, 2222, 30, 500], [4, 55, 6777, 1]]
+print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
