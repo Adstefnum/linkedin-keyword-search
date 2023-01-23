5a66623fc2482f3e2f09ced9e2a6aaf049be47bf
Stephen Adesina <bauerstevenson@gmail.com>
2023-01-23 08:35:48 +0000

    updating parser logic

--- src/linkedin_parser.py
+++ src/linkedin_parser.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from lxml import etree
 
-NAME_XPATH ='//*[@id="vOBumQanRr62leWLbm4DBA=="]/div/ul/li[1]/div/div/div[2]/div[1]/div[1]/div/span[1]/span/a/span/span[1]/text()' 
+NAME_XPATH ='//div/ul/li[1]/div/div/div[2]/div[1]/div[1]/div/span[1]/span/a/span/span[1]' 
 NAME_CLASS = 'app-aware-link '
 ID_XPATH = '//*[@id="vOBumQanRr62leWLbm4DBA=="]/div/ul/li[1]/div/div/div[2]/div[1]/div[1]/div/span[1]/span/a'#get the href and then split by / and then by ?
 COMPANY_XPATH ='//*[@id="vOBumQanRr62leWLbm4DBA=="]/div/ul/li[3]/div/div/div[2]/div[1]/div[2]/div[1]/text()'
@@ -34,5 +34,15 @@ class LinkedinParser:
         #in each of those wrappers, find the name, ID, company
         #append to the results list as a [ID, Name, Company]
 
+with open('./outputs/soup.html','r') as file:
+    soup = file.read()
+    beaut = BeautifulSoup(soup, features ='html.parser')
+    results = beaut.find(class_=RESULTS_CLASS)
+    names_and_ids = results.find_all(class_="entity-result__title-text t-16")
+    print(names_and_ids)
+    companies = results.find_all(class_="entity-result__primary-subtitle t-14 t-black t-normal")
+
+    # parser = LinkedinParser(soup)
+    # print(parser.get_value_of_text_element_using_xpath(NAME_XPATH))
 
 
